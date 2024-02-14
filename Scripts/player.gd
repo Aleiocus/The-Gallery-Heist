9c6878e46d7b82f1519a61b465ff4614280f8c94
Val <147787102+Valsprites@users.noreply.github.com>
2024-02-14 05:11:57 -0500

    dash implimented

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,15 +5,17 @@ extends CharacterBody2D
 const _max_move_speed : float = 200.0
 const _accel : float = 180.0
 const _decel : float = 400.0
-const _jump_force : float = 200.0
+const _jump_force : float = 250.0
 var _gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 const _slide_speed : float = 55
-const _wall_jump_force : float = 200
-const _pushoff_force : float = 100.0
+const _wall_jump_force : float = 240
+const _pushoff_force : float = 130.0
+const _dash_force: float = 375
 
 const _death_height_y : float = 150.0
 @onready var _coyote_timer : Timer = $CoyoteTimer
 @onready var _jump_buffer_timer : Timer = $JumpBufferTimer
+@onready var _dash_timer = $DashTimer
 var _state_machine : StateMachine = StateMachine.new()
 
 # Setup signals
@@ -29,7 +31,7 @@ var _item : PickupItem = null
 func _ready():
 	PlayerManager.player = self
 	_state_machine.add_state("normal", Callable(), Callable(), Callable(), _state_normal_ph_process)
-	_state_machine.add_state("dash", Callable(), Callable(), Callable(), Callable())
+	_state_machine.add_state("dash", Callable(), Callable(), Callable(), _state_dash_ph_process)
 	_state_machine.add_state("wall_slide", Callable(), Callable(), Callable(), _state_wall_slide_ph_process)
 	_state_machine.change_state("normal")
 
@@ -83,12 +85,17 @@ func _state_normal_ph_process(delta : float):
 			velocity.y = -_jump_force
 	
 	if is_on_wall() == true \
-		and is_on_floor() == false:
+		and is_on_floor() == false\
+		and velocity.y != 0 :
 		_state_machine.change_state("wall_slide")
 	
 	# Fall too far and die
 	if global_position.y > _death_height_y:
 		_game_over()
+	
+	if Input.is_action_just_pressed("dash"):
+		_state_machine.change_state("dash")
+
 
 func _game_over():
 	# Currently set to restart level, can be changed easily here.
@@ -114,3 +121,15 @@ func _state_wall_slide_ph_process(delta: float):
 			velocity.y = -_wall_jump_force
 			velocity.x = _pushoff_force
 			_state_machine.change_state("normal")
+
+func _state_dash_ph_process(delta: float):
+	var _direction = global_position.direction_to(get_global_mouse_position())
+	velocity = _direction * _dash_force 
+	_dash_timer.start()
+	var dashing: bool = true
+	if _dash_timer.is_stopped() == false:
+		_state_machine.change_state("normal")
+	if is_on_wall():
+		_state_machine.change_state("wall_slide")
+	move_and_slide()
+
