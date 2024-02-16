31ed16b4712c81e111a28062393d3e14a3fd355c
Val <147787102+Valsprites@users.noreply.github.com>
2024-02-15 19:24:27 -0500

    Updated player camera
    now position of follow will smoothly tween. Tweens slower when on ground than in air to give quicker feedback while platforming

--- Scripts/player_camera.gd
+++ Scripts/player_camera.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+var _in_air : bool
+
 const _look_ahead_factor = 0.2
 var _facing = 0
 @onready var _prev_camera_pos = get_target_position()
@@ -9,16 +11,23 @@ func _process(delta : float):
 	_check_facing()
 	_prev_camera_pos = get_target_position()
 
-
 func _check_facing():
 	var new_facing = sign(get_target_position().x - _prev_camera_pos.x)
 	if new_facing != 0 && _facing != new_facing:
 		_facing = new_facing
 		var target_offset = get_viewport_rect().size.x * _look_ahead_factor * _facing
-		position.x = target_offset
+		if _in_air == false:
+			var tween = create_tween()
+			tween.tween_property(self, "position:x", target_offset, 1)
+		elif _in_air == true:
+			var tween = create_tween()
+			tween.tween_property(self, "position:x", target_offset, .75)
+
 
 func _on_player_is_in_air():
 	drag_vertical_enabled = true
+	_in_air = true
 
 func _on_player_is_on_ground():
 	drag_vertical_enabled = false
+	_in_air = false
