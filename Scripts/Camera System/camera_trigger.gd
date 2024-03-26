c8d49d169fe704f87d72b42a131587c20b70ed2f
Dissonant Void <arandomdude75@gmail.com>
2024-03-26 21:19:53 +0000

    reworked level camera and camera triggers

--- Scripts/Camera System/camera_trigger.gd
+++ Scripts/Camera System/camera_trigger.gd
@@ -1,41 +1,101 @@
 @tool
+class_name CameraTrigger
 extends Area2D
 
-@export var _is_starting_state : bool = false # only 1 trigger can be set as default, this is not enforced so if multiple triggers have this on the last one in the tree will apply
+@export var _change_state : bool = false :
+	set(value):
+		_change_state = value
+		notify_property_list_changed()
+@export var _change_zoom : bool = false :
+	set(value):
+		_change_zoom = value
+		notify_property_list_changed()
+@export var _change_lock : bool = false :
+	set(value):
+		_change_lock = value
+		notify_property_list_changed()
+
+@export_group("Camera State")
 @export var _trigger_state : LevelCamera.CameraState :
 	set(value):
 		_trigger_state = value
 		notify_property_list_changed()
-@export var _target_zoom : Vector2 = Vector2.ONE
-
 @export var _idle_position : Vector2
-@export var _clamped_limits : Rect2
 
+@export_group("Axis Lock")
+@export var _release_x_bound : bool = true :
+	set(value):
+		_release_x_bound = value
+		notify_property_list_changed()
+@export var _release_y_bound : bool = true :
+	set(value):
+		_release_y_bound = value
+		notify_property_list_changed()
+@export var _x_bound : Vector2
+@export var _y_bound : Vector2
 
-func _ready():
-	if Engine.is_editor_hint() == false && _is_starting_state:
-		await get_tree().process_frame
-		_apply_camera_state()
+@export_group("Camera Properties")
+@export var _zoom : Vector2 = Vector2.ONE
+
 
 func _on_body_entered(body : Node2D):
-	if body is Player && _trigger_state != World.level.level_camera.get_state():
-		_apply_camera_state()
+	if body is Player:
+		apply_camera_state()
 
-func _apply_camera_state():
-	match _trigger_state:
-		LevelCamera.CameraState.idle:
-			World.level.level_camera.change_state_idle(_idle_position, _target_zoom)
-		LevelCamera.CameraState.clamped:
-			World.level.level_camera.change_state_clamped(_clamped_limits, _target_zoom)
-		LevelCamera.CameraState.free:
-			World.level.level_camera.change_state_free(_target_zoom)
+func apply_camera_state():
+	var camera : LevelCamera = World.level.level_camera
+	
+	if _change_state == false && _change_zoom == false && _change_lock == false:
+		push_warning("Camera trigger has no effect")
+	
+	if _change_state:
+		match _trigger_state:
+			LevelCamera.CameraState.idle:
+				camera.set_state_idle(_idle_position)
+			LevelCamera.CameraState.follow:
+				camera.set_state_follow()
+	
+	if _change_zoom:
+		camera.set_target_zoom(_zoom)
+	
+	if _change_lock:
+		if _release_x_bound == true:
+			camera.remove_axis_bound(true)
+		elif _release_x_bound == false:
+			camera.set_axis_bound(true, _x_bound)
+		
+		if _release_y_bound == true:
+			camera.remove_axis_bound(false)
+		elif _release_y_bound == false:
+			camera.set_axis_bound(false, _y_bound)
 
 func _validate_property(property: Dictionary):
-	# TODO: hide properties instead of disabling them
-	if (property["name"] == "_idle_position" &&
-	_trigger_state != LevelCamera.CameraState.idle):
-		property["usage"] |= PROPERTY_USAGE_READ_ONLY
+	var hide_prop : Callable = func():
+		property["usage"] = PROPERTY_USAGE_NO_EDITOR
 	
-	elif (property["name"] == "_clamped_limits" &&
-	_trigger_state != LevelCamera.CameraState.clamped):
-		property["usage"] |= PROPERTY_USAGE_READ_ONLY
+	# hide properties that aren't used
+	match property["name"]:
+		"_trigger_state", "_idle_position":
+			if _change_state == false:
+				hide_prop.call()
+			
+			if property["name"] == "_idle_position":
+				if _trigger_state != LevelCamera.CameraState.idle:
+					hide_prop.call()
+		
+		"_zoom":
+			if _change_zoom == false:
+				hide_prop.call()
+		
+		"_x_bound", "_y_bound", "_release_x_bound", "_release_y_bound":
+			if _change_lock == false:
+				hide_prop.call()
+		
+			else:
+				if property["name"] == "_x_bound":
+					if _release_x_bound:
+						hide_prop.call()
+				
+				elif property["name"] == "_y_bound":
+					if _release_y_bound:
+						hide_prop.call()
--- Scripts/Camera System/level_camera.gd
+++ Scripts/Camera System/level_camera.gd
@@ -2,75 +2,75 @@ class_name LevelCamera
 extends Camera2D
 
 # TODO: addon to show camera constraints when a trigger node is selected
-enum CameraState {idle, clamped, free}
+enum CameraState {idle, follow}
 enum ShakeLevel {low, medium, high}
 
-# TEMP: forces free state if other states are giving you a headach
-@export var _debug_force_free : bool = false
+@export var _starting_trigger : CameraTrigger
 
 @onready var _shake_timer : Timer = $ShakeTimer
 
-var _curr_state : CameraState
-const _speed : float = 110.0
+var _curr_state : CameraState = CameraState.follow
 const _zoom_speed : float = 1.6
 var _target_zoom : Vector2 = Vector2.ONE
-
-const _look_ahead_offset : float = 22.0
-const _player_velocity_factor : float = 0.6
-const _distance_factor : float = 1.4
 var _target_position : Vector2
-var _clamp_limits : Rect2
+const _speed : float = 120.0
+const _distance_speed_factor : Vector2 = Vector2(0.022, 0.5)
+
+const _player_velocity_factor : Vector2 = Vector2(0.7, 0.0)
+const _player_velocity_max_offset : float = 140.0
+
+var _axis_bounds : Dictionary
 
 const _shake_data : Dictionary = {
-	ShakeLevel.low:{"max_offset":1.0},
+	ShakeLevel.low:   {"max_offset":1.0},
 	ShakeLevel.medium:{"max_offset":4.0},
-	ShakeLevel.high:{"max_offset":9.0}
+	ShakeLevel.high:  {"max_offset":9.0}
 }
 var _curr_shake_level : ShakeLevel
 
 
 func _ready():
-	if _debug_force_free:
-		change_state_free(Vector2.ONE)
+	if _starting_trigger:
+		await get_tree().process_frame # wait for World.level to be set
+		_starting_trigger.apply_camera_state()
 
 func _process(delta : float):
+	var player : Player = World.level.player
 	# update zoom
 	if zoom != _target_zoom:
 		zoom.x = move_toward(zoom.x, _target_zoom.x, _zoom_speed * delta)
 		zoom.y = move_toward(zoom.y, _target_zoom.y, _zoom_speed * delta)
-	var player : Player = World.level.player
 	
-	# calc camera movement speed
-	var distance_speed_modifier : float
-	var final_speed : float
-	if _curr_state == CameraState.idle:
-		distance_speed_modifier =\
-			global_position.distance_to(_target_position) * _distance_factor
-		final_speed = (_speed + distance_speed_modifier) * delta
-	elif _curr_state == CameraState.clamped || _curr_state == CameraState.free:
-		distance_speed_modifier =\
-			global_position.distance_to(player.global_position) * _distance_factor
-		final_speed = (_speed + distance_speed_modifier) * delta
-	
-	# calc destination
+	# calc target_position
 	var target_position : Vector2
 	if _curr_state == CameraState.idle:
 		target_position = _target_position
-	elif _curr_state == CameraState.clamped || _curr_state == CameraState.free:
-		var velocity_offset : float = player.velocity.length() * _player_velocity_factor
-		target_position = Vector2(
-			player.global_position.x + sign(player.velocity.x) * (_look_ahead_offset + velocity_offset),
-			player.global_position.y + sign(player.velocity.y) * (velocity_offset)
-		)
+		
+	elif _curr_state == CameraState.follow:
+		var velocity_offset : Vector2 = player.velocity.abs() * _player_velocity_factor
+		velocity_offset.x = min(velocity_offset.x, _player_velocity_max_offset)
+		velocity_offset.y = min(velocity_offset.y, _player_velocity_max_offset)
+		
+		target_position.x = player.global_position.x + sign(player.velocity.x) * (velocity_offset.x)
+		target_position.y = player.global_position.y + sign(player.velocity.y) * (velocity_offset.y)
 	
-	if _curr_state == CameraState.clamped:
-		# clamp
-		target_position.x = clamp(target_position.x, _clamp_limits.position.x, _clamp_limits.size.x)
-		target_position.y = clamp(target_position.y, _clamp_limits.position.y, _clamp_limits.size.y)
+	# bounds
+	if _axis_bounds.has("x"):
+		target_position.x = clamp(target_position.x, _axis_bounds["x"][0], _axis_bounds["x"][1])
+	if _axis_bounds.has("y"):
+		target_position.y = clamp(target_position.y, _axis_bounds["y"][0], _axis_bounds["y"][1])
+	
+	# calc camera speed
+	# speed is influenced by the camera's distance to target. the further away the faster we move to catch up
+	var distance : float = global_position.distance_to(target_position)
+	var final_speed : Vector2 = Vector2(
+		_speed * distance * _distance_speed_factor.x * delta,
+		_speed * distance * _distance_speed_factor.y * delta
+	)
 	
 	global_position = Vector2(
-		move_toward(global_position.x, target_position.x, final_speed),
-		move_toward(global_position.y, target_position.y, final_speed)
+		move_toward(global_position.x, target_position.x, final_speed.x),
+		move_toward(global_position.y, target_position.y, final_speed.y)
 	)
 	
 	# shake
@@ -85,26 +85,22 @@ func shake(shake_level : ShakeLevel, duration : float):
 	_shake_timer.wait_time = duration
 	_shake_timer.start()
 
-func get_state() -> CameraState:
-	return _curr_state
-
-func change_state_idle(position_ : Vector2, zoom_ : Vector2):
-	if _debug_force_free: return
-	
-	_target_position = position_
+func set_target_zoom(zoom_ : Vector2):
 	_target_zoom = zoom_
+
+func set_axis_bound(is_x : bool, lock : Vector2):
+	_axis_bounds["x" if is_x else "y"] = lock
+
+func remove_axis_bound(is_x : bool):
+	if is_x && _axis_bounds.has("x"): _axis_bounds.erase("x")
+	elif is_x == false && _axis_bounds.has("y"): _axis_bounds.erase("y")
+
+func set_state_idle(position_ : Vector2):
+	_target_position = position_
 	_curr_state = CameraState.idle
 
-func change_state_clamped(limits : Rect2, zoom_ : Vector2):
-	if _debug_force_free: return
-	
-	_clamp_limits = limits
-	_target_zoom = zoom_
-	_curr_state = CameraState.clamped
-
-func change_state_free(zoom_ : Vector2):
-	_target_zoom = zoom_
-	_curr_state = CameraState.free
+func set_state_follow():
+	_curr_state = CameraState.follow
 
 func _on_shake_timer_timeout():
 	offset = Vector2.ZERO
